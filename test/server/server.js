366c3d0dc4334794cd5c0c0301b36beaf1d2395d
Bryan Ashby <bryan@l33t.codes>
2017-09-22 14:41:04 -0600

    tabs -> spaces

--- test/server/server.js
+++ test/server/server.js
@@ -1,25 +1,17 @@
 /* jslint node: true */
 'use strict';
 
-const paths			= require('path');
+const paths         = require('path');
 const SocketCluster = require('socketcluster').SocketCluster;
-//const fs			= require('fs');
 
 const serverOptions = {
-	workers				: 1,
-	brokers				: 1,
-	port				: 8000,
-	appName				: 'scio_beast test server',
-	workerController	: paths.join(__dirname, 'worker.js'),
-	protocol			: 'http',
-
-	perMessageDeflate	: true,
-
-/*
-	authPrivateKey		: fs.readFileSync(paths.join(__dirname, 'private.pem'), 'utf8'),
-	authPublicKey		: fs.readFileSync(paths.join(__dirname, 'public.pem'), 'utf8'),
-	authAlgorithm		: 'RS256',	
-*/	
+    workers             : 1,
+    brokers             : 1,
+    port                : 8000,
+    appName             : 'scio_beast test server',
+    workerController    : paths.join(__dirname, 'worker.js'),
+    protocol            : 'http',
+    perMessageDeflate   : true,
 };
 
 const socketCluster = new SocketCluster(serverOptions);
--- test/server/worker.js
+++ test/server/worker.js
@@ -2,34 +2,34 @@
 'use strict';
 
 function logEvent(name, eventData) {
-	console.log(`${name}: ${JSON.stringify(eventData)}`);
+    console.log(`${name}: ${JSON.stringify(eventData)}`);
 }
 
 const respGotIt = { got_it : true };
 
 module.exports.run = function(worker) {
-	console.log(`Worker startup with PID: ${process.pid}`);
+    console.log(`Worker startup with PID: ${process.pid}`);
 
-	const scServer = worker.scServer;
+    const scServer = worker.scServer;
 
-	scServer.on('connection', socket => {
+    scServer.on('connection', socket => {
 
-		socket.on('event_no_resp', eventData => {
-			logEvent('event_no_resp', eventData);
-		});
+        socket.on('event_no_resp', eventData => {
+            logEvent('event_no_resp', eventData);
+        });
 
-		socket.on('event_with_resp', (eventData, resp) => {
-			logEvent('event_with_resp', eventData);
+        socket.on('event_with_resp', (eventData, resp) => {
+            logEvent('event_with_resp', eventData);
 
-			return resp(null, respGotIt);
-		});
+            return resp(null, respGotIt);
+        });
 
-		socket.on('event_with_timed_out_resp', (eventData, resp) => {
-			logEvent('event_with_timed_out_resp', eventData);
+        socket.on('event_with_timed_out_resp', (eventData, resp) => {
+            logEvent('event_with_timed_out_resp', eventData);
 
-			setTimeout( () => {
-				return resp(null, respGotIt);
-			}, eventData.ackTimeout + 5000);
-		});
-	});
+            setTimeout( () => {
+                return resp(null, respGotIt);
+            }, eventData.ackTimeout + 5000);
+        });
+    });
 }
